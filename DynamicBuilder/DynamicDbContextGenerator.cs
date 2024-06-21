128d7c084947b70b18c1ac9e6e70fa32ec614e2d
韦增迅 <weizengxun@live.cn>
2024-06-21 19:16:41 +0800

    update

--- DynamicBuilder/DynamicDbContextGenerator.cs
+++ DynamicBuilder/DynamicDbContextGenerator.cs
@@ -4,111 +4,192 @@ using Microsoft.CodeAnalysis.Emit;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Migrations.Design;
 using Microsoft.Extensions.DependencyInjection;
 using Pomelo.EntityFrameworkCore.MySql.Design.Internal;
 using System.Reflection;
 using System.Runtime.Loader;
+using System.Text;
 
-namespace DynamicBuilder;
+namespace DynamicSpace;
 
-internal class DynamicDbContextGenerator
+public class DynamicDbContextGenerator
 {
-
-
-
-    public DbContext? BuildDbContext()
-    {
-        throw new NotImplementedException();
-    }
-
-    public static Type? CompileCode()
-    {
-
-        var assemblyName = "DynamicDbContextAssembly";
-        var str = $@"
+    private readonly static string MigrationDirectory = Path.Combine("Migrations");
+    private readonly static string ModelDirectory = Path.Combine("Models");//
+    private readonly static string assemblyName = "DynamicDbContextAssembly";
+    private readonly static string str = $@"
 using System;
 using System.Linq;
 using System.Reflection;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-namespace {assemblyName};
+//using {assemblyName}.Models;
+//namespace {assemblyName};
 
 public abstract class EntityBase {{ }}
 
-public class Book:EntityBase
-{{
-    public int BookId {{ get; set; }}
-    public string Title {{ get; set; }}
-    //public string Description {{ get; set; }}
-
-    //public string Text {{ get; set; }}
-    //public string Text2 {{ get; set; }}
-    //public string Text3 {{ get; set; }}
-    //public string Text4 {{ get; set; }}
-    //public string Text5 {{ get; set; }}
-
-    public DateTime Created {{ get; set; }}
-
-    //public DateTime Updated {{ get; set; }}
-    public int AuthorId {{ get; set; }}
-    public Author Author {{ get; set; }}
-}}
-public class Author:EntityBase
-{{
-    public int AuthorId {{ get; set; }}
-    public string FirstName {{ get; set; }}
-    public string LastName {{ get; set; }}
-    public ICollection<Book> Books {{ get; set; }} // = new List<Book>();
-}}
-
-public class ApplicationDbContext : DbContext
+public class DynamicDbContext : DbContext
 {{
 
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    public DynamicDbContext(DbContextOptions<DynamicDbContext> options) : base(options)
     {{
     }}
-    //public DbSet<Book> Books {{ get; set; }}
-    //public DbSet<Author> Authors {{ get; set; }}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {{
-        modelBuilder.Entity<Book>().ToTable(""books"");
-        var entityTypes = Assembly.Load(""DynamicDbContextAssembly"")
+        var entityTypes = Assembly.GetExecutingAssembly()
              .GetTypes()
-             .Where(t => t.IsAssignableFrom(typeof(EntityBase)) && !t.IsAbstract).ToList();
+             .Where(t => typeof(EntityBase).IsAssignableFrom(t) && !t.IsAbstract)
+             .ToList();
 
         foreach (var t in entityTypes)
         {{
-            var paramters = new Type[] {{ t }};
-            var builder = (EntityTypeBuilder)typeof(ModelBuilder).GetMethod(""Entity"", paramters)?.Invoke(modelBuilder, paramters);
-            builder?.ToTable(t.Name + ""s"");
+             var builder = typeof(ModelBuilder).GetMethod(""Entity"", [])?
+                    .MakeGenericMethod(t)?
+                    .Invoke(modelBuilder, null) as EntityTypeBuilder ?? throw new Exception(""modelBuilder.Entity<T> 失败"");
+
+                builder.ToTable(t.Name + ""s"");
         }}
 
         base.OnModelCreating(modelBuilder);
     }}
 }}
 ";
+    private AssemblyLoadContext _context;
+    private DbContext _dbContext;
+    private bool hasPendingModelChanges = false;
+    //private Compilation _compilation;
+    private readonly Dictionary<string, string> sourceCodeDict = new Dictionary<string, string> { { "DynamicDbContext.cs", str } };
+
+    public DynamicDbContextGenerator()
+    {
+        this._context = new AssemblyLoadContext(Guid.NewGuid().ToString(), true);
+        Build();
+    }
+
+    public DynamicDbContextGenerator AddEntity(string entityName, string entityPropertiesCode)
+    {
+        //var name = entityName.ToLower();
+        var sb = new StringBuilder();
+        sb.AppendLine("using System;");
+        //sb.AppendLine($"namespace {assemblyName}.Models;");
+        sb.AppendLine($"public class {entityName} : EntityBase{{");
+        sb.AppendLine(entityPropertiesCode);
+        sb.AppendLine("}");
+
+        var entityFileName = Path.Combine(ModelDirectory, entityName + ".cs");
+        AddSourceCode(entityFileName, sb.ToString());
+        hasPendingModelChanges = true;
+        return this;
+    }
+
+    private IMigrationsScaffolder GetMigrationsScaffolder(DbContext dbContext)
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddEntityFrameworkDesignTimeServices();
+        serviceCollection.AddDbContextDesignTimeServices(dbContext);
+        new MySqlDesignTimeServices().ConfigureDesignTimeServices(serviceCollection);
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        var migrationsScaffolder = serviceProvider.GetRequiredService<IMigrationsScaffolder>();
+
+        return migrationsScaffolder;
+    }
+
+    public DynamicDbContextGenerator AddMigration(string? name = null)
+    {
+        EnsureBuild();
+        using var dbContext = this._dbContext!;
+
+        if (!dbContext.Database.HasPendingModelChanges()) return this;
+
+        var migrationName = name ?? DateTime.Now.Ticks.ToString();
+        using (var scope = dbContext.GetService<IServiceScopeFactory>().CreateScope())
+        {
+
+            var scaffolder = GetMigrationsScaffolder(dbContext);
+            var migration = scaffolder.ScaffoldMigration(migrationName, rootNamespace: assemblyName);
+
+            var modelSnapshotFileName = Path.Combine(MigrationDirectory, migration.SnapshotName, migration.FileExtension);
+            var migrationFileName = Path.Combine(MigrationDirectory, migration.MigrationId, migration.FileExtension);
+            var migrationMetadataFileName = Path.Combine(MigrationDirectory, migration.MigrationId, ".Designer" + migration.FileExtension);
+
+            AddSourceCode(modelSnapshotFileName, migration.SnapshotCode);
+            AddSourceCode(migrationFileName, migration.MigrationCode);
+            AddSourceCode(migrationMetadataFileName, migration.MetadataCode);
+
+            return this;
+        }
+    }
+
+    public void UpdateDatabase()
+    {
+        try
+        {
+            using var dbContext = this._dbContext;
+            var migrator = dbContext.GetService<IMigrator>();
+            migrator.Migrate();
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+
+    public void Build()
+    {
+        var syntaxTrees = sourceCodeDict.Select(item =>
+        {
+            return CSharpSyntaxTree.ParseText(item.Value).WithFilePath(item.Key);
+        });
+        this._dbContext?.Dispose();
+        var compilation = CreateCompilation();
+        compilation = compilation.AddSyntaxTrees(syntaxTrees);
+        var assembly = BuildAssembly(compilation);
+        this._dbContext = BuildDbContext(assembly);
+        this.hasPendingModelChanges = false;
+    }
+
+    private void EnsureBuild()
+    {
+        if (hasPendingModelChanges) Build();
+    }
+
+    private static Compilation CreateCompilation()
+    {
         var references = new[] {
             MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
-            MetadataReference.CreateFromFile(typeof(ICollection<>).Assembly.Location),
             MetadataReference.CreateFromFile(Assembly.Load("System").Location),
             MetadataReference.CreateFromFile(Assembly.Load("System.Reflection").Location),
             MetadataReference.CreateFromFile(Assembly.Load("System.Collections").Location),
             MetadataReference.CreateFromFile(Assembly.Load("System.Linq").Location),
+            MetadataReference.CreateFromFile(Assembly.Load("System.Linq.Expressions").Location),
             MetadataReference.CreateFromFile(Assembly.Load("System.Runtime").Location),
             MetadataReference.CreateFromFile(Assembly.Load("Microsoft.EntityFrameworkCore.Relational").Location),
+            MetadataReference.CreateFromFile(Assembly.Load("Pomelo.EntityFrameworkCore.MySql").Location),
             MetadataReference.CreateFromFile(typeof(DbContext).Assembly.Location)
         };
-        var syntaxTrees = new[] { str }.Select(s => CSharpSyntaxTree.ParseText(s));
 
         Compilation compilation = CSharpCompilation.Create(assemblyName)
             .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, optimizationLevel: OptimizationLevel.Release))
-            .AddReferences(references)
-            .AddSyntaxTrees(syntaxTrees);
+            .AddReferences(references);
 
+        return compilation;
+    }
+
+    private void AddSourceCode(string key, string code)
+    {
+        if (!sourceCodeDict.TryAdd(key, code))
+        {
+            sourceCodeDict.Add(key, code);
+        }
+    }
+
+
+    private Assembly BuildAssembly(Compilation compilation)
+    {
         using var ms = new MemoryStream();
         EmitResult result = compilation.Emit(ms);
         ms.Seek(0, SeekOrigin.Begin);
@@ -119,25 +200,29 @@ public class ApplicationDbContext : DbContext
             foreach (Diagnostic diagnostic in errors)
             {
                 Console.WriteLine(diagnostic.ToString());
-                return null;
+                throw new Exception("Compilation error");
             }
         }
 
         Console.WriteLine($"Successfully created Assembly: {assemblyName}");
 
-        Assembly assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
+        if (_context != null)
+        {
+            _context.Unload();
+            _context = new AssemblyLoadContext(Guid.NewGuid().ToString(), true);
+        }
 
-        var a1 = Assembly.GetExecutingAssembly();
-        var a2 = Assembly.GetCallingAssembly();
-        var a3 = Assembly.GetEntryAssembly();
+        return _context!.LoadFromStream(ms);
+    }
 
-        var bookType = assembly.GetTypes().FirstOrDefault(m => m.Name == "Book");
-        var dbContextType = assembly.GetTypes().FirstOrDefault(m => m.Name == "ApplicationDbContext");
+    private static DbContext BuildDbContext(Assembly assembly)
+    {
+        if (assembly == null) throw new NullReferenceException("assembly is null");
+
+        var dbContextType = assembly.GetTypes().FirstOrDefault(m => m.Name == "DynamicDbContext") ?? throw new NullReferenceException("DynamicDbContext 为空");
         var serverVersion = new MySqlServerVersion(Version.Parse("8.4.0"));
         var optionsBuilderType = typeof(DbContextOptionsBuilder<>).MakeGenericType(dbContextType);
-        var optionsBuilderInstance = Activator.CreateInstance(optionsBuilderType);
-
-        if (optionsBuilderInstance == null) throw new ArgumentException("OptionsBuilderInstance 为空");
+        var optionsBuilderInstance = Activator.CreateInstance(optionsBuilderType) ?? throw new NullReferenceException("OptionsBuilderInstance 为空");
 
         // Get the UseMySql extension method
         optionsBuilderInstance = typeof(MySqlDbContextOptionsBuilderExtensions)?
@@ -148,45 +233,16 @@ public class ApplicationDbContext : DbContext
         var optionsValue = optionsBuilderType.GetProperty("Options", dbContextOptionsType)?.GetValue(optionsBuilderInstance);
         var constructor = dbContextType.GetConstructor(new[] { dbContextOptionsType });
 
-        if (constructor == null || optionsValue == null || bookType == null)
+        if (constructor == null || optionsValue == null)
         {
             throw new ArgumentException("constructor 为空");
         }
 
-        var context = (DbContext)constructor.Invoke([optionsValue]);
-
-
-
-        var query = dbContextType.GetMethod("Set", 1, [])?
-                .MakeGenericMethod(bookType)?
-                .Invoke(context, null); // DbSet<T>
-
-        var queryable = typeof(DbSet<>).MakeGenericType(bookType)?
-            .GetMethod("AsQueryable")?
-            .Invoke(query, []); //IQueryable<T>
-
-        var list = typeof(Enumerable).GetMethod("ToList")?
-            .MakeGenericMethod(bookType)
-            .Invoke(null, [queryable]);
-
-        var serviceCollection = new ServiceCollection();
-        serviceCollection.AddEntityFrameworkDesignTimeServices();
-        serviceCollection.AddDbContextDesignTimeServices(context);
-        MySqlDesignTimeServices? designTimeServices = new();
-        designTimeServices.ConfigureDesignTimeServices(serviceCollection);
-
-
-        var serviceProvider = serviceCollection.BuildServiceProvider();
-        using var scope = serviceProvider.CreateScope();
-        var scaffolder = scope.ServiceProvider.GetRequiredService<IMigrationsScaffolder>();
-        //var migrationsAssembly = scope.ServiceProvider.GetRequiredService<IMigrationsAssembly>();
-
-        var fiels = scaffolder.RemoveMigration("./migrations", null, true, null);
-
-        Console.WriteLine("assembly.GetType() {}" + assembly.GetType().Name);
-
-        return assembly.GetType();
-
+        return (DbContext)constructor.Invoke([optionsValue]);
     }
+
+
+
+
 }
 
